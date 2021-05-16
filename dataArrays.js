3333621c77174a28dcefdda80c924033770903ae
118140184 Alfha RIdho Akira <60598672+118140184@users.noreply.github.com>
2021-05-16 21:39:53 +0700

    Add files via upload
    perubahan data array

---
+++ dataArrays.js
@@ -0,0 +1,173 @@
+export const categories = [
+  {
+    id: 3,
+    name: 'Masakan Padang',
+    photo_url:
+    'https://cdn-cas.orami.co.id/parenting/images/menu_padang_hero.original.jpegquality-90.jpg'
+  },
+  {
+    id: 1,
+    name: 'Masakan Bandung',
+    photo_url: 'https://lh3.googleusercontent.com/proxy/WOj0wGtNhMUroJSIscCW-XTCIXpuNchwntGl32yjss9XW12E6a9jJUH3TaZA0Ky1VKc_kGpqfIqificNYFSB4RkKMv2pupEIgmeXTB_F9iHVFOSLVekxdStcLkVOuJzgGL3xJ8yvVXp1XEt7Stg1rCMJVoHJlenj-BATjHGuGrJWbFgjXCtbwnoDHK_1RyUWliC_lF05KSa0dcE'
+  },
+  {
+    id: 2,
+    name: 'Masakan Lampung',
+    photo_url:
+      'https://guratgarut.com/wp-content/uploads/2020/03/makanan-khas-lampung.jpg'
+  },
+  {
+    id: 4,
+    name: 'Masakan Bali',
+    photo_url:
+    'https://asset.kompas.com/crops/TiyXB0FIKRLMHKxEuJAzUGC8a6Q=/78x0:664x390/750x500/data/photo/2018/04/16/2032270773.jpg'
+  },
+];
+
+export const recipes = [
+  {
+    recipeId: 122,
+    categoryId: 3,
+    title: 'Rendang Daging',
+    photo_url: 'https://cdn-brilio-net.akamaized.net/news/2019/10/18/172502/1113624-1000xauto-resep-masakan-padang-asli.jpg',
+    photosArray: [
+      'https://cdn-brilio-net.akamaized.net/news/2019/10/18/172502/1113624-1000xauto-resep-masakan-padang-asli.jpg',
+      'https://cdn-2.tstatic.net/tribunnews/foto/bank/images/resep-rendang.jpg',
+      'https://cdns.klimg.com/merdeka.com/i/w/news/2021/05/04/1304153/640x320/5-resep-bumbu-rendang-yang-lezat-dan-menggugah-selera.jpg',
+    ],
+    time: '320',
+    description:
+      'Bahan-bahan :\n - 1 kg daging sapi\n - Baby kentang\n - 2 lembar daun kunyit\n - 1 batang sereh\n - 4 lembar daun jeruk\n - 1 lembar daun salam\n - 1 sdm ambu2 (boleh skip)\n - 1 ons cabai giling halus\n - 1 kg santan kental\n - 10 bawang merah ukuran besar\n - 5 bawang putih\n - Jahe (sebesar ibu jari)\n - Lengkuas muda (sebesar ibu jari)\n - 1 paket rempah gulai/rendang (ketumbar, kapulaga, bunga pekak, cengkeh, kayu manis dan buah pala)\n - Garam (sesuai selera)\n\n Cara memasak :\n - Masukkan semua bahan dalam satu wajan/kuali kecuali daging, kentang dan ambu-ambu (kelapa kering khas masakan Padang).\n - Aduk sebentar hingga santan sedikit mendidih.\n - Masukkan daging dan ambu-ambu.\n - Jika rendang sudah meletup-letup, masukan kentang.\n- Aduk lagi hingga rendang tanak (mengeluarkan minyak) dan rendang menghitam.'
+  },
+  {
+    recipeId: 3,
+    categoryId: 4,
+    title: 'Sate Lilit Bali',
+    photo_url:
+      'https://cdn-brilio-net.akamaized.net/news/2020/03/24/181044/1195943-1000xauto-resep-makanan-bali.jpg',
+    photosArray: [
+      'https://cdn-brilio-net.akamaized.net/news/2020/03/24/181044/1195943-1000xauto-resep-makanan-bali.jpg',
+    ],
+    time: '30',
+    description: 'Bahan-bahan:\n - 350 gram daging ayam, giling halus\n - 1 sdm santan kental\n - 1 sdt garam\n - 1 sdt gula\n - 1,5 sdm kelapa muda parut\n - 150 gram udang, giling\n - 15-20 batang serai untuk tusukan\n\n Bumbu halus:\n - 1 buah serai ambil putihnya\n - 1 cm jahe\n - 1/2 sdt terasi, bakar\n - 1/4 sdt ketumbar, sangrai\n - 3 butir kemiri, sangrai\n - 3 cm kunyit, bakar\n - 3 lembar daun jeruk\n - 3 siung bawang putih\n - 5 buah cabai keriting\n - 8 siung bawang merah\n\n Cara membuat:\n - Haluskan bumbu, kemudian tumis hingga harum.\n - Tambahkan gula garam, aduk hingga matang, angkat.\n - Pada wadah masukkan ayam, udang giling, santan, kelapa parut lalu tambahkan bumbu halus yang telah ditumis.\n - Ambil 1 sdm adonan, lilitkan pada ujung serai atau bambu\n - Panggang di atas pan, bolak balik hingga matang dan harum.\n - Sate lilit siap disajikan.'
+  },
+  {
+    recipeId: 2,
+    categoryId: 3,
+    title: 'Gulai daun singkong',
+    photo_url: 'https://cdn-brilio-net.akamaized.net/news/2019/10/18/172502/1113629-1000xauto-resep-masakan-padang-asli.jpg',
+    photosArray: [
+      'https://cdn-brilio-net.akamaized.net/news/2019/10/18/172502/1113629-1000xauto-resep-masakan-padang-asli.jpg',
+     
+      
+    ],
+    time: '30',
+    description:
+      'Bahan-bahan:\n - Daun singkong direbus dan peras, lalu potong-potong\n - Daun salam\n - Lengkuas\n - Serei\n - Rawit utuh\n - Udang kering\n- Teri Medan\n - Santan\n - Bawang merah\n - Bawang putih\n - Cabai merah\n - Terasi\n- Kunyit\n - Kemiri\n\n Cara membuat:\n - Tumis bumbu halus sampai wangi, baru masukkan ebi aduk, tuang santan encer masukkan daun singkong, penyedap rasa kalau Sudah mendidih.\n - Masukkan santan kental, kecilkan api, aduk terus jangan sampai pecah santannya, aduk sampai mendidih, matikan api dan angkat.'
+  },
+  {
+    recipeId: 3,
+    categoryId: 3,
+    title: 'Ikan siam sambal bawang',
+    photo_url:
+      'https://cdn-brilio-net.akamaized.net/news/2019/10/18/172502/1113626-1000xauto-resep-masakan-padang-asli.jpg',
+    photosArray: [
+      'https://cdn-brilio-net.akamaized.net/news/2019/10/18/172502/1113626-1000xauto-resep-masakan-padang-asli.jpg',
+    ],
+    time: '60',
+    description:
+      'Bahan-bahan:\n - 1/4 kg ikan gabus/ikan siam\n - 10 bawang merah\n - 1 bawang putih\n - 1 buah bawang bombai\n - 2 buah tomat\n - 1 ons cabai setan /rawit merah\n - 1 ons cabai rawit hijau\n - Garam (sesuai selera)\n\n Cara membuat:\n - Cuci bersih ikan siam lalu rendam dengan air panas, cuci lagi dan tiriskan dan goreng.\n - Tumis bawang merah iris, setelah harum masukkan bawang bombai dan bawang putih, setelah bawang layu masukkan duo cabai serta potongan tomat.\n - Bumbui dengan garam dan penyedap.\n - Biarkan sambal dingin dan campurkan dengan ikan yang sudah digoreng.'
+  },
+  {
+    recipeId: 3,
+    categoryId: 4,
+    title: 'Rujak bulung',
+    photo_url:
+      'https://cdn-brilio-net.akamaized.net/news/2020/03/24/181044/1195938-1000xauto-resep-makanan-bali.jpg',
+    photosArray: [
+      'https://cdn-brilio-net.akamaized.net/news/2020/03/24/181044/1195938-1000xauto-resep-makanan-bali.jpg',
+    ],
+    time: '30',
+    description: 'Bahan-bahan:\n - 300 gram bulung buni atau rumput laut segar\n - 1 sendok sayur kuah pindang\n - Air untuk merebus\n - Air es untuk merendam\n\n Bumbu kelapa :\n - 1/4 butir kelapa yang dibakar (parut)\n - 1 cm lengkuas (parut)\n\n Campur kedua bahan lalu tambahkan kuah pindang.\n\n  Bumbu rujak bulung:\n - 2 buah cabe merah atau sesuai selera\n - Terasi bakar secukupnya\n - Garam sesuai selera\n - Gula pasir secukupnya\n - Ulek seluruh bahan hingga halus.\n\n Cara menyajikan rujak bulung :\n - Didihkan air, masukkan bulung buni sebentar saja.\n - Angkat dan langsung masukkan pada air es. Tiriskan.\n - Tata rujak bulung ini diatas piring.\n - Campur bumbu kelapa dan bumbu rujak, taruh diatas bulung buni.\n - Beri perasan jeruk limau.\n - Rujak bulung siap disajikan.'
+  },
+  {
+    recipeId: 1,
+    categoryId: 3,
+    title: 'Balado jengkol',
+    photo_url: 'https://cdn-brilio-net.akamaized.net/news/2019/10/18/172502/1113628-1000xauto-resep-masakan-padang-asli.jpg',
+    photosArray: [
+      'https://cdn-brilio-net.akamaized.net/news/2019/10/18/172502/1113628-1000xauto-resep-masakan-padang-asli.jpg',
+    ],
+    time: '30',
+    description:
+      'Bahan-bahan:\n - 250 gr jengkol, belah dua\n - 2 buah kentang kupas, potong-potong\n - 50 gr teri\n - 1 buah tomat, iris\n - Minyak dan garam secukupnya\n - 100 gr cabai keriting\n - 5 buah cabai rawit\n - 7 siung bawang merah\n\n Cara membuat:\n - Goreng kentang sampai matang lalu tiriskan.\n - Goreng jengkol, lalu masukkan ke mangkok berisi air kemudian tiriskan lalu geprek.\n - Goreng teri sampai matang, tiriskan.\n - Tumis bumbu halus dan tomat sampai matang, beri garam secukupnya.\n - Icip rasa, matikan api. Tuang bahan-bahan yang sudah digoreng ke tumisan cabai. Aduk rata.\n - Sajikan dengan nasi hangat'
+  
+  },
+  {
+    recipeId: 4,
+    categoryId: 1,
+    title: 'Soto Bandung',
+    photo_url: 'https://cdn-brilio-net.akamaized.net/news/2020/07/08/187924/1264654-1000xauto-resep-makanan-bandung.jpg',
+    photosArray: [
+      'https://cdn-brilio-net.akamaized.net/news/2020/07/08/187924/1264654-1000xauto-resep-makanan-bandung.jpg',
+    ],
+    time: '90',
+    description:
+      'Bahan:\n - 350 gram daging bagian sengkel (potong-potong)\n - 1 buah lobak kecil (potong tipis)\n - 1 batang sereh, geprek\n - 1 lembar daun salam\n - 3 lembar daun jeruk\n - 1 ruas lengkuas, geprek\n - 1 ruas jahe, geprek\n - garam dan kaldu bubuk secukupnya\n - air 500 ml dan 1000 ml\n - minyak goreng\n\n Bumbu halus:\n - 5 siung bawang merah\n - 2 siung bawang putih\n - 1/2 sdt lada butir\n\n Pelengkap:\n - kacang kedelai (rendam ± 1 jam, goreng)\n - emping\n - sambal rawit\n - seledri\n - bawang goreng\n - jeruk nipis\n - lobak yang sudah direbus\n\n Cara membuat:\n- Rebus daging. Diamkan selama 30 menit.\n - Angkat daging, cuci bersih.\n - Rebus daging lagi dengan campuran serai, jahe, lengkuas, daun salam, dan daun jeruk.\n - Tumis bumbu halus, tuang ke rebusan daging. Beri garam dan kaldu bubuk.\n - Sajikan soto dengan pelengkap.'
+  },
+  {
+    recipeId: 5,
+    categoryId: 1,
+    title: 'Mi Kocok',
+    photo_url:
+      'https://cdn-brilio-net.akamaized.net/news/2020/07/08/187924/1264655-1000xauto-resep-makanan-bandung.jpg',
+    photosArray: [
+      'https://cdn-brilio-net.akamaized.net/news/2020/07/08/187924/1264655-1000xauto-resep-makanan-bandung.jpg',
+    ],
+    time: 45,
+    description:
+      'Bahan:\n - 500 gram kaki sapi\n - 200 gram mi gepeng siap pakai\n - 100 gram tauge\n\n Bahan kuah kaldu:\n - 4 liter air\n - 6 siung bawang putih\n - 50 gram kemiri\n - 2 sdm garam\n - 1 sdm kaldu sapi bubuk\n - 1 sdt merica bubuk\n - 2 batang serai (memarkan)\n\n Taburan pelengkap:\n - 2 batang daun seledri\n - 1 batang daun bawang\n - 3 sdm bawang goreng\n - 3 buah jeruk limau\n - saus sambal secukupnya\n - kecap manis secukupnya\n\n Sambal:\n -50 gram cabai rawit merah\n\n Cara membuat:\n - Rebus kaki sapi, potong bentuk dadu.\n - Rebus mi gepeng, tiriskan.\n - Siram tauge dengan air panas, tiriskan.\n - Tumis bawang putih dan kemiri yang sudah dihaluskan. Masukkan ke rebusan air. Beri garam, kaldu bubuk, merica, dan serai.\n- Goreng cabai rawit, haluskan dengan 125 ml air panas.\n - Iris daun bawang dan saledri.\n - Tata mi gepeng, kikil, tauge, daun bawang, saledri, bawang goreng dan perasan jeruk limau.\n - Siram dengan kuah kaldu. Beri saus sambal, kecap manis, dan sambal cabai rawit.'
+  },
+  {
+    recipeId: 6,
+    categoryId: 2,
+    title: 'Seruit',
+    photo_url:
+      'https://cdn-brilio-net.akamaized.net/news/2019/10/29/173033/1118628-1000xauto-makanan-khas-lampung.jpg',
+    photosArray: [
+      'https://cdn-brilio-net.akamaized.net/news/2019/10/29/173033/1118628-1000xauto-makanan-khas-lampung.jpg',
+    ],
+    time: 30,
+    description:
+      'Bahan:\n - 1 kg ikan nila atau sesuai selera\n - 5 butir cabe merah\n - 3 siung bawang merah\n - 1 sendok makan minyak goreng\n\n Bahan sambel seruit:\n - 50 gram cabai merah\n - 2 block terasi\n - 50 gram cabai rawit\n - 2 buah jeruk sate\n - 1 sdt garam\n - 200 gram rampai (tomat kecil)\n\n Cara memasak:\n - Bersihkan ikan, sisihkan\n - Haluskan cabai merah, bawang merah, dan garam. Campurkan dengan ikan. Tuang minyak, aduk rata. Diamkan selama 15 menit. Panggang ikan sampai matang\n - Untuk sambal, campur semua bahan sambal, masukkan tomat kecil dan digiling sampai halus. Iris jeruk sate, peras airnya masukkan ke dalam sambal.'
+
+  },
+  {
+    recipeId: 7,
+    categoryId: 2,
+    title: 'Gulai taboh',
+    photo_url: 'https://cdn-brilio-net.akamaized.net/news/2019/10/29/173033/1118627-1000xauto-makanan-khas-lampung.jpg',
+    photosArray: [
+      'https://cdn-brilio-net.akamaized.net/news/2019/10/29/173033/1118627-1000xauto-makanan-khas-lampung.jpg',
+    ],
+    time: 45,
+    description:
+      'Bahan:\n - 500 gram ikan tuna\n - 3 butir kemiri (haluskan)\n - 5 buah belimbing wuluh (iris)\n - 8 siung bawang merah (haluskan)\n - 4 siung bawang putih (haluskan)\n - 10 buah cabai rawit (sesuai selera)\n - garam secukupnya\n - kunyit secukupnya (haluskan)\n - 1 batang serai (geprek)\n - 200 ml santan kental\n - lengkuas secukupnya (geprek)\n - kemangi secukupnya (siangi)\n - jeruk nipis/lemon\n - gula secukupnya\n - penyedap (opsional)\n\n Cara memasak:\n - Cuci bersih ikan tuna, lumuri air perasan jeruk nipis/lemon dan biarkan selama 5 menit\n - Tumis bawang merah, bawang putih, cabai, kunyit, kemiri, dan garam. Masukkan serai dan laos, tumi hingga matang\n - Tuang air, masukkan ikan, beri gula dan penyedap. Masak selama 5 menit. Tuang santan bersama belimbing wuluh. Tunggu hingga mendidih, aduk perlahan agar santan tidak pecah. Terakhir masukan daun kemangi.'
+
+  },
+  {
+    recipeId: 8,
+    categoryId: 2,
+    title: 'Pindang Patin',
+    photo_url: 'https://cdn-brilio-net.akamaized.net/news/2019/10/29/173033/1118633-1000xauto-makanan-khas-lampung.jpg',
+    photosArray: [
+      'https://cdn-brilio-net.akamaized.net/news/2019/10/29/173033/1118633-1000xauto-makanan-khas-lampung.jpg',
+    ],
+    time: 60,
+    description:
+      'Bahan:\n - 3 potong ikan patin\n - 2 lembar daun salam\n - 2 sdm air asam jawa\n - 3 sdm air perasan jeruk nipis\n - 2 lembar daun jeruk\n - 1 buah daun bawang, potong-potong\n - 1 batang sereh, geprek lalu simpulkan\n - 1 ruas lengkuas, memarkan\n - 7 buah cabai rawit merah, potong serong\n - 1 sdt gula pasir\n - 1/2 sdt kaldu bubuk\n - 1 sdt garam\n - 1 buah tomat merah, potong-potong\n - 50 ml minyak goreng\n - 500 ml air\n\n Bumbu halus:\n - 6 siung bawang merah\n - 3 buah cabai merah keriting\n - 1 ruas jahe\n - 3 siung bawang putih\n - 1 ruas kunyit\n\n Cara memasak:\n - Lumuri ikan patin dengan air perasan jeruk nipis, diamkan selama 15 menit\n - Tumis bumbu halus, masukkan daun salam, daun jeruk, sereh, dan lengkuas\n - Tuang sedikit air, beri gula, garam, kaldu bubuk, dan air asam jawa\n - Masukkan ikan patin dan potongan cabai rawit merah. Aduk sebentar, tunggu hingga bumbu meresap lalu tuangi air lagi\n - Masukkan potongan tomat dan daun bawang. Panaskan sebentar.'
+
+  }
+];
+
+

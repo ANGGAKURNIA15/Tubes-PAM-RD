ff3c4f4f1f47b811bce7c33fe98b8b32b92bba0b
aldipubiangga <55964234+aldipubiangga@users.noreply.github.com>
2021-05-16 21:31:08 +0700

    update final

--- src/screens/ListResep/styles.js
+++ src/screens/ListResep/styles.js
@@ -6,19 +6,7 @@ const styles = StyleSheet.create({
   photo: Resepmasakan.photo,
   title: Resepmasakan.title,
   category: Resepmasakan.category,
-  indikator: {
-    marginLeft: 30,
-    marginTop: 15,
-    marginRight: 30,
-    width: 420,
-    height: 150,
-    borderWidth: 0.5,
-    borderRadius: 15,
-    marginVertical: 10,
-    marginHorizontal: 5,
-    padding: 10,
-    flexDirection: "row",
-  },
+  indikator: Resepmasakan.indikator
 });
 
 export default styles;
